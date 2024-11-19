2586706353bf2619a98191daace4796c2bb116b4
Rowan12628 <170511926+Rowan12628@users.noreply.github.com>
2024-11-19 15:47:40 +0000

    new problem solved

--- index.js
+++ index.js
@@ -77,3 +77,25 @@ function otherAngle(a, b) {
   let twoAngle = a + b;
   return 180 - twoAngle;
 }
+
+// Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+
+// Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+
+function lovefunc(flower1, flower2) {
+  function isEven(num) {
+    if (num % 2 == 0) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+  let flower1Even = isEven(flower1);
+  let flower2Even = isEven(flower2);
+
+  if (flower1Even + flower2Even == 1) {
+    return true;
+  } else {
+    return false;
+  }
+}
